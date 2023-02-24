29f8827558cd89228a3d62c68ece36982bdbfe6e
maiarafrancine186 <maiarafrancine186@gmail.com>
2023-02-24 19:41:42 -0300

    commit node 2

---
+++ index.js
@@ -0,0 +1,64 @@
+const users = [] 
+
+const checkUserId = (request,response, next)=>{
+ 
+  const { id } = request.params  
+ 
+  const index = users.findIndex(user => user.id === id) 
+ 
+  if (index < 0) {    
+    
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.userIndex= index 
+  request.userId= id
+
+  next()
+}
+      
+     
+
+app.get('/users', (request, response) => { 
+  return response.json(users)
+})
+
+app.post('/users', (request, response) => { 
+  const { name, age } = request.body        
+
+
+  const user = { id: uuid.v4(), name, age }
+
+  users.push(user)
+
+  return response.status(201).json(user) 
+  
+})
+
+app.put('/users/:id',checkUserId,  (request, response) => {  
+ 
+  const { name, age } = request.body
+  const index = request.userIndex 
+  const id = request.userId
+  const updateUser = { id, name, age }
+   
+  users[index] = updateUser 
+  
+  return response.json(updateUser)
+})
+
+
+app.delete('/users/:id',checkUserId,  (request, response) => {
+  const index = request.userIndex
+  
+  users.splice(index, 1) 
+
+  return response.status(204).json()
+
+})
+
+app.listen( 3000,() => {
+  console.log("server started on port ")
+
+})
+
